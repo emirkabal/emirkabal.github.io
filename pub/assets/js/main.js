d438817feaa6b33257c76b408b7f1d6cfc72e8b0
Emir Kabal <kabalemir@gmail.com>
2020-04-29 01:49:45 +0300

    Update main.js

--- pub/assets/js/main.js
+++ pub/assets/js/main.js
@@ -10,12 +10,6 @@ window.addEventListener('scroll', function() {
 let keyList = [];
 
     $(document).ready(function () {
-      if (!["emirkabal.com", "www.emirkabal.com", "localhost"].includes(window.document.domain)) {
-        $('html').empty();
-        window.location.href = "https://emirkabal.com";
-        return;
-      }
-
       $('#dervis').on('ended', function () {
         $('#cheat1').hide();
       });
